13767e11763f812691c16dc62ab54deced7b4d18
Hasan Emin Balkaya <hebalkaya@gmail.com>
2026-04-10 23:57:34 +0200

    Initiated simulate.py and cited papers for fragment lengths.

---
+++ src/simulate.py
@@ -0,0 +1,13 @@
+"""
+Generates synthetic cfDNA datasets with known tumor fractions.
+
+Biological basis (from literature):
+* Healthy cfDNA: fragment lengths ~167bp (nucleosomal peak, the length of DNA associated with a nucleosome plus linker)
+* Tumour cfDNA: enriched ~143bp (sub-nucleosomal, altered chromatin)
+* Cancer methylation: hypermetilation at tumour specific CpG islands
+
+Parameters derived from / Source papers:
+* Underhill et al. 2016, PLOS (mouse cfDNA & ctDNA fragment lengths) (Fragment Length of Circulating Tumor DNA)
+* Snyder et al. 2016, Cell (cfDNA fragment lengths) (Cell-free DNA Comprises an In Vivo Nucleosome Footprint that Informs Its Tissues-Of-Origin)
+* Hanahan & Weinberg 2011 (methylation as cancer hallmark)
+"""
