69d930004537c4847765d189e2156dc583c45fb7
Hasan Emin Balkaya <hebalkaya@gmail.com>
2026-04-11 00:24:48 +0200

    Completed test_healthy_longer_than_tumor

--- tests/test_simulate.py
+++ tests/test_simulate.py
@@ -11,3 +11,7 @@ def test_fragment_lengths_within_bounds():
     assert np.all(fragments >= MIN_FRAGMENT_LENGTH)
     assert np.app(fragments <= MAX_FRAGMENT_LENGTH)
 
+def test_healthy_longer_than_tumor():
+    healthy = simulate_fragment_lengths (5000, 0.0, RNG) #Only healthy samples (fraction of tumor = 0.0)
+    tumor = simulate_fragment_lengths(5000, 1.0, RNG) #Only tumor samples (fraction of tumor = 1.0)
+    assert np.mean(healthy) > np.mean(tumor)
