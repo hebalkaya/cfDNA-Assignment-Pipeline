04a432571d9eba060e703e240e4a0d5711d13e07
Hasan Emin Balkaya <hebalkaya@gmail.com>
2026-04-11 00:22:26 +0200

    Set seed to 42. Completed test_fragment_lengths_within_bounds

--- tests/test_simulate.py
+++ tests/test_simulate.py
@@ -1,1 +1,13 @@
 """Tests for simulate.py"""
+import numpy as np
+import pytest
+from src.simulate import *
+
+RNG = np.random.default_rng(42) # Setting seed for reproducibility
+
+def test_fragment_lengths_within_bounds():
+    """Making sure all fragments are within min fragment and max fragment bounds"""
+    fragments = simulate_fragment_lengths(1000, 0.0, RNG)
+    assert np.all(fragments >= MIN_FRAGMENT_LENGTH)
+    assert np.app(fragments <= MAX_FRAGMENT_LENGTH)
+
