5f0a80d2431c15684413998e197202ef76a5ffd2
Hasan Emin Balkaya <hebalkaya@gmail.com>
2026-04-11 16:55:36 +0200

    included dependencies and the biologically defined windows in bp as GLOBAL VARIABLES

--- src/fragmentomics.py
+++ src/fragmentomics.py
@@ -14,4 +14,20 @@ Source papers:
     * Underhill et al. 2016, PLOS (Mouse cfDNA & ctDNA fragment lengths) (Fragment Length of Circulating Tumor DNA)
     * Snyder et al. 2016, Cell (cfDNA fragment lengths) (Cell-free DNA Comprises an In Vivo Nucleosome Footprint that Informs Its Tissues-Of-Origin)
     * Cristiano et al., Nature 2019 (Genome-wide cell-free DNA fragmentation in patients with cancer)
+
+See the code for biologically defined windows
 """
+
+import numpy as np
+import pandas as pd
+from typing import List, Dict
+from src.simulate import SampleData
+
+# Biologically defined windows (bp)
+SHORT_THRESHOLD = 150           # fragments below 150bp are "short"
+LONG_THRESHOLD = 180            # fragments above 180bp are "long"
+NUCLEOSOMAL_LOW = 160           # nucleosomal window lower boundary
+NUCLEOSOMAL_HIGH = 180          # nucelosomal window upper boundary
+SUB_NUCLEOSOMAL_LOW = 120       # sub-nucleosomal window lower boundary
+SUB_NUCLEOSOMAL_HIGH = 150      # sub-nucleosomal window upper boundary
+ENTROPY_BINS = 50               # bins for entropy calculation
