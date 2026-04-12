b449098dfba3ea1defc128966de1a186d0f3e16b
Hasan Emin Balkaya <hebalkaya@gmail.com>
2026-04-12 10:07:43 +0200

    test_mean_methylation_in_range completed. All 30 tests pass.

--- tests/test_methylation.py
+++ tests/test_methylation.py
@@ -7,3 +7,7 @@ from src.methylation import *
 HEALTHY_SAMPLE = simulate_dataset([0.0], n_samples_per_fraction = 1, seed = 42)[0]
 TUMOR_SAMPLE = simulate_dataset([1,0], n_samples_per_fraction = 1, seed = 42)[0]
 
+def test_mean_methylation_in_range():
+    """Making sure methylation is within the expected range (0.0 - 1.0)"""
+    val = mean_methylation(HEALTHY_SAMPLE.methylation_values)
+    assert 0.0 <= val <= 1.0
